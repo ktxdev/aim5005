46a7dc8c3f7b5d310875f13705fdc0564fdc3e8b
Sean Huvaya <sean.ktxdev@gmail.com>
2024-09-23 20:45:10 -0400

    Implement StandardScaler

--- aim5005/features.py
+++ aim5005/features.py
@@ -42,4 +42,25 @@ class MinMaxScaler:
 class StandardScaler:
     def __init__(self):
         self.mean = None
-        raise NotImplementedError
+        self.variance = None
+
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+
+    def fit(self, x:np.ndarray) -> None:
+        x = self._check_is_array(x)
+        self.mean = np.mean(x, axis=0)
+        self.variance = np.std(x, axis=0)
+
+    def transform(self, x:np.ndarray) -> np.ndarray:
+        x = self._check_is_array(x)
+        return (x - self.mean) / self.variance
+
+    def fit_transform(self, x:list) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
